a28349628f3ead3ad21c9c23c40fcd93e94d2740
Umair Abbasi <umair.abbasi@fourfridays.com>
2018-10-21 15:17:58 -0700

    updated urls.py to match clean installation config

--- fourfridays/urls.py
+++ fourfridays/urls.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import, unicode_literals
-
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
@@ -20,15 +18,22 @@ urlpatterns = [
 
     url(r'^search/$', search_views.search, name='search'),
 
-    url('^sitemap\.xml$', sitemap),
+    url(r'^sitemap\.xml$', sitemap),
+
+    # For anything not caught by a more specific rule above, hand over to
+    # Wagtail's page serving mechanism. This should be the last pattern in
+    # the list:
+    url(r'', include(wagtail_urls)),
+
+    # Alternatively, if you want Wagtail pages to be served from a subpath
+    # of your site, rather than the site root:
+    #    url(r'^pages/', include(wagtail_urls)),
 ]
 
 
 if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-    from django.views.generic import TemplateView
-    from django.views.generic.base import RedirectView
 
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
@@ -39,8 +44,3 @@ if settings.DEBUG:
         url(r'^test404/$', TemplateView.as_view(template_name='404.html')),
         url(r'^test500/$', TemplateView.as_view(template_name='500.html')),
     ]
-
-
-urlpatterns += [
-    url(r'', include(wagtail_urls)),
-]

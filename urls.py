aff5ab46f66ee9b0f6acb5558830f5ced2409a11
Umair Abbasi <umair.abbasi@fourfridays.com>
2019-10-13 13:46:13 -0700

    #24an45[closed] hubspot api
    Integrating form submissions with our CRM.

--- urls.py
+++ urls.py
@@ -3,9 +3,12 @@ from django.conf.urls import url, include
 from aldryn_django.utils import i18n_patterns
 import aldryn_addons.urls
 
+from pages import views as page_views
+
 
 urlpatterns = [
-    # add your own patterns here
+    url(r'^sales/$', page_views.sales_inquiry_form),
+    url(r'^sales/inquiry/', page_views.sales_inquiry_hubspot_form),
 ] + aldryn_addons.urls.patterns() + i18n_patterns(
     # add your own i18n patterns here
     *aldryn_addons.urls.i18n_patterns()  # MUST be the last entry!

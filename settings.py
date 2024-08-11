7737993629ac04cfd7b557fab6c7d5a3f55ab22a
Umair Abbasi <umair.abbasi@fourfridays.com>
2024-08-11 18:35:02 +0200

    added conn_max_age option to dj_database_url

--- settings.py
+++ settings.py
@@ -111,7 +111,11 @@ WSGI_APPLICATION = "wsgi.application"
 
 DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite://:memory:")
 
-DATABASES = {"default": dj_database_url.parse(DATABASE_URL)}
+DATABASES = {"default": dj_database_url.config(
+    default=DATABASE_URL,
+    conn_max_age=600,
+    conn_health_checks=True,
+)}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
